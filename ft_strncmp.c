f8ec00d4f22cd45b89c92b6b1926f7ac8b1414b4
Xenia-42 <xviladri@cbr10s5.42barcelona.com>
2024-10-14 04:45:01 +0200

    add

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By:  xviladri < xviladri@student.42barcelona.c +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:37:20 by xviladri          #+#    #+#             */
-/*   Updated: 2024/10/13 20:58:33 by xviladri         ###   ########.fr       */
+/*   Updated: 2024/10/14 04:44:51 by xviladri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
 
+	if (!s1 || !s2)
+		return (0);
+	if (n == 0)
+		return (0);
 	i = 0;
 	while (i < n && (s1[i] || s2[i]))
 	{
