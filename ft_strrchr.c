4381f6f12c212ae9adb43d7d9a0f2d9e66b7f4c6
Xenia-42 <xviladri@cbr10s5.42barcelona.com>
2024-10-14 04:37:27 +0200

    add

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By:  xviladri < xviladri@student.42barcelona.c +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:59:54 by xviladri          #+#    #+#             */
-/*   Updated: 2024/10/13 21:06:52 by xviladri         ###   ########.fr       */
+/*   Updated: 2024/10/14 04:37:12 by xviladri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ char	*ft_strrchr(const char *s, int c)
 	int	i;
 	int	len;
 
-	if (s == NULL)
-		return (NULL);
 	len = ft_strlen((char *)s);
 	if (c == '\0')
-		return ((char *)&s[len]);
+		return ((char *)s + len);
+	if (s == NULL)
+		return (NULL);
 	i = len - 1;
-	while (i > 0 && s[i])
+	while (i >= 0 && s[i])
 	{
 		if (s[i] != ((unsigned char)c))
 			i--;
