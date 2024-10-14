34869098c7425ab86c613400adfe2ea67969336c
Xenia-42 <xviladri@cbr10s5.42barcelona.com>
2024-10-14 05:17:34 +0200

    add

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By:  xviladri < xviladri@student.42barcelona.c +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 12:47:30 by xviladri          #+#    #+#             */
-/*   Updated: 2024/10/13 21:22:19 by xviladri         ###   ########.fr       */
+/*   Updated: 2024/10/14 05:17:22 by xviladri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,21 @@
 char	*ft_strdup(const char *s)
 {
 	char	*p;
-	size_t	len;
+	int		len;
+	int		i;
 
-	len = ft_strlen((char *)s);
+	len = 0;
+	while (s[len])
+		len++;
 	p = (char *)malloc((len + 1) * sizeof(char));
 	if (!p)
-		return (0);
-	ft_memcpy(p, s, len);
-	p[len] = '\0';
-	return (0);
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		c[i] = s[i];
+		i++;
+	}
+	c[i] = '\0';
+	return (c);
 }
