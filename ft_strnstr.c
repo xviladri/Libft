d720efb2619038ddf0e2a883b0049fe8c0b58efd
Xenia-42 <xviladri@cbr10s5.42barcelona.com>
2024-10-14 05:05:53 +0200

    add

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By:  xviladri < xviladri@student.42barcelona.c +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 17:12:11 by xviladri          #+#    #+#             */
-/*   Updated: 2024/10/13 21:03:00 by xviladri         ###   ########.fr       */
+/*   Updated: 2024/10/14 05:05:38 by xviladri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,21 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
+	size_t	n;
 
-	i = 0;
-	if (little == NULL || little[0] == '\0')
+	if (!big)
+		return (0);
+	if (*little == '\0')
 		return ((char *)big);
+	i = 0;
+	n = ft_strlen(little);
 	while (big[i] && i < len)
 	{
-		if (big[i] == little[0])
+		j = 0;
+		while (little[j] == big[i + j] && (i + j) < len)
 		{
-			j = 0;
-			while (little[j] && big[i + j] && (i + j) < len)
-			{
-				if (big[i + j] == little[j])
-					j++;
-			}
-			if (little[j] == '\0')
+			j++;
+			if (n == j)
 				return ((char *)&big[i]);
 		}
 		i++;
